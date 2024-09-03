89abcc8cc679e4a512de15c6e840bf6f4b7cbfd1
bblur-ux <bblur4235@gmail.com>
2024-09-03 15:21:16 +0100

    Add files via upload

---
+++ send_sms.php
@@ -0,0 +1,42 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $phone_numbers = explode("\n", trim($_POST['phone_numbers']));
+    $carrier = $_POST['carrier'];
+    $message = $_POST['message'];
+
+    // Carrier email-to-SMS gateways
+    $carrier_gateways = array(
+        'att' => '@txt.att.net',
+        'tmobile' => '@tmomail.net',
+        'verizon' => '@vtext.com',
+        'sprint' => '@messaging.sprintpcs.com',
+    );
+
+    if (!array_key_exists($carrier, $carrier_gateways)) {
+        die('Unsupported carrier');
+    }
+
+    $from_email = 'sesmsrec@bestinallsecureverygoodverificationboostconfidentinall.hannahjonesjewels.com'; 
+    $reply_to_email = 'sesmsrec@bestinallsecureverygoodverificationboostconfidentinall.hannahjonesjewels.com'; 
+
+    foreach ($phone_numbers as $phone_number) {
+        $phone_number = trim($phone_number);
+        if (!preg_match('/^\d{10}$/', $phone_number)) {
+            echo "Invalid phone number format: $phone_number. Please enter a 10-digit phone number.<br>";
+            continue;
+        }
+
+        $to_number = $phone_number . $carrier_gateways[$carrier];
+        $subject = 'SMS via Email';
+        $headers = 'From: ' . $from_email . "\r\n" .
+                   'Reply-To: ' . $reply_to_email . "\r\n" .
+                   'X-Mailer: PHP/' . phpversion();
+
+        if (mail($to_number, $subject, $message, $headers)) {
+            echo "SMS sent successfully to $phone_number!<br>";
+        } else {
+            echo "Failed to send SMS to $phone_number.<br>";
+        }
+    }
+}
+?>
